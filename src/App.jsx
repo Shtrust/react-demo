598055739f2aa7971f35edf12d6a65edd07e07c3
Ben <bensaine09@gmail.com>
2024-05-04 14:37:20 -0600

    switch to vite and update deps

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>WOOHOO! YOU DID IT!</h2><p>Send '<b>🧸🫣🚣</b>' in Discord if you got this far! (iykyk)</p>
-        <small>
-          Edit <code>src/App.js</code> and save to reload.
-        </small>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h2>WOOHOO! YOU DID IT!</h2>
+				<p>If you got this far, it means you properly configured your environment and this project</p>
+				<small>
+					Edit <code>src/App.js</code> and save to reload.
+				</small>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
