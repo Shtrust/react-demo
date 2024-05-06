60b3c05b6e56d8e20dd57adaaa0bc01bdd100b9e
Shtrust <104174257+Shtrust@users.noreply.github.com>
2024-05-05 19:42:47 -0500

    You need to reformat this garbage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import logo from "./logo.svg"
 import "./App.css"
-
+/*
 function App() {
 	return (
 		<div className="App">
@@ -15,5 +15,66 @@ function App() {
 		</div>
 	)
 }
+
+
+*/
+
+function App() {
+	return (
+		<div className="page">		
+			<Header></Header>
+			<div className="centerx">
+				<Middlecomp></Middlecomp>
+			</div>
+		</div>
+	);
+}
+
+function Header() {
+	return (
+	<header className="header">
+		<p className="title">My Stopwatch</p>
+		<p className="tag">By <a href="https://github.com/Shtrust" target="_blank">@Shtrust</a></p>
+	</header>);
+}
+
+function Startbutton() {
+	return (
+		<button className="start">START</button>
+	);
+}
+
+function Stopbutton() {
+	return (
+		<button className="stop">STOP</button>	
+		);
+}
+
+function Startstop() {
+	return (
+		<div className="butcont">
+			<Startbutton></Startbutton>
+			<Stopbutton></Stopbutton>
+		</div>
+	);
+}
+
+function Stopwatch() {
+	return (
+		<div className="time">
+			00:00
+		</div>
+	);
+}
+
+function Middlecomp() {
+	return (
+		<div className="display">
+			<Stopwatch></Stopwatch>
+			<Startstop></Startstop>
+		</div>
+	);
+}
+
 
 export default App
